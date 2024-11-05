801a30fe64e6556dcaedd5de46fabe93a419c30b
Manish Prasad Gupta <144058549+ManishPrasadGupta@users.noreply.github.com>
2024-11-05 05:27:44 +0000

    added one projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import PigGame from './Card/PigGame'
 
 function Projects() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md w-full">
+      <h2 className="text-2xl font-bold mb-4">My Skills</h2>
+      <div className="flex space-x-4">
+        <PigGame />
+      </div>
+      <div className="flex space-x-4">
+        
+      </div>
     </div>
   )
 }
