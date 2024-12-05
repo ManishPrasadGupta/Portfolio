a9a9ebe184d41638f914f60a3228a1c0e808f3bd
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2024-12-05 13:18:35 +0530

    small change

--- src/components/Projects/Card/PigGame.jsx
+++ src/components/Projects/Card/PigGame.jsx
@@ -18,6 +18,7 @@ function PigGame() {
                     
                     <a
                         href="https://pig-game-sigma-three.vercel.app/"
+                        target="_blank"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         View
