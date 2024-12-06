3d9ea18d46ff40db518f6da9c6f76a036583d511
manishprasad7173@gmail.com <manishprasad7173@gmail.com>
2024-12-06 21:01:36 +0530

    added one more project

--- src/components/Projects/Card/SnakeGame.jsx
+++ src/components/Projects/Card/SnakeGame.jsx
@@ -5,19 +5,19 @@ function PigGame() {
         <div className="flex flex-col items-center p-6 bg-gray-400 rounded-lg shadow-md w-full">
           <div className="flex space-x-4">
           <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="https://pig-game-sigma-three.vercel.app/">
-                <img className="p-8 rounded-t-lg" src="https://res.cloudinary.com/mnisprsd/image/upload/v1733384676/Screenshot_20241205_121146_gvt330.png" alt="product_image1" />
+            <a href="https://snake-game-brown-eight.vercel.app/">
+                <img className="p-8 rounded-t-lg" src="https://res.cloudinary.com/mnisprsd/image/upload/v1733498817/Screenshot_20241206_205242_k9nerh.png" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
-                <a href="https://pig-game-sigma-three.vercel.app/">
+                <a href="https://snake-game-brown-eight.vercel.app/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                    A fun and interactive dice-based game built with HTML, CSS, and JavaScript. The Pig Game is a two-player game where players take turns rolling a dice, accumulating points, and strategizing to win by reaching the target score first.
+                    A classic Snake game built with HTML, CSS, and JavaScript, featuring a scoreboard, username input, and a toggle music on/off button for an enhanced gaming experience.
                     </h5>
                 </a>
                 <div className="flex items-center justify-between">
                     
                     <a
-                        href="https://pig-game-sigma-three.vercel.app/"
+                        href="https://snake-game-brown-eight.vercel.app/"
                         target="_blank"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PigGame from './Card/PigGame'
+import SnakeGame from './Card/SnakeGame'
 
 function Projects() {
   return (
-    <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md w-full">
+    <div className="flex flex-col items-center p-6 bg-indigo-300 rounded-lg shadow-md w-full">
       <h2 className="text-2xl font-bold mb-4">My Skills</h2>
       <div className="flex space-x-4">
         <PigGame />
+        <SnakeGame />
       </div>
       <div className="flex space-x-4">
         <h1>More projects coming soon...</h1>
